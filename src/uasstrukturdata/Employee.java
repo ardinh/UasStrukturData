e10ebb35f6f76a809083bb1a3c813da3300ac500
ardinaufanhadian <ardinaufanhadian@gmail.com>
2023-01-27 10:53:12 +0700

    first Commit

---
+++ src/uasstrukturdata/Employee.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uasstrukturdata;
+
+/**
+ *
+ * @author ardin
+ */
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employee other = (Employee) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
---
+++ src/uasstrukturdata/UasStrukturData.java
@@ -0,0 +1,258 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uasstrukturdata;
+
+import java.util.*;
+
+/**
+ *
+ * @author ardin
+ */
+public class UasStrukturData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void firstTask() {
+        int noSoal = 0;
+        Scanner masukanSoal = new Scanner(System.in);
+        String soal = "1. Stack\n" + "2.Queue\n";
+        System.out.println(soal);
+        System.out.print("Masukan Pilihan : ");
+        noSoal = masukanSoal.nextInt();
+        switch (noSoal) {
+            case 1:
+                ArrayStack stack = new ArrayStack(5);
+
+                System.out.println("Is Stack empty => " + stack.isEmpty() + "\n");
+
+                System.out.println("Adding Data to Stack");
+                System.out.println("=============================================================================================================================================");
+                stack.push(new Employee("Ardi Naufan", "Hadian", 1));
+                stack.push(new Employee("Nando", "Nandi", 2));
+                stack.push(new Employee("Ente", "Anna", 3));
+                stack.push(new Employee("Dian", "Silva", 4));
+                stack.push(new Employee("Alvin", "Salvina", 5));
+                stack.printStack();
+                System.out.println("Size of Stack => " + stack.size());
+                System.out.println("=============================================================================================================================================");
+                System.out.println("See First Data From Stack => " + stack.peek());
+                System.out.println("Remove Data From Stack => " + stack.pop());
+
+                System.out.println("=============================================================================================================================================");
+                System.out.println("Last After All Remove");
+                stack.printStack();
+                System.out.println("=============================================================================================================================================");
+                System.out.println("\n\n");
+                next();
+                break;
+            case 2:
+                Employee janeJones = new Employee("ardi", "naufan", 123);
+                Employee johnDoe = new Employee("lala", "lili", 4567);
+                Employee marySmith = new Employee("lulu", "lele", 22);
+                Employee mikeWilson = new Employee("mama", "mia", 3245);
+                Employee billEnd = new Employee("Bill", "gunawan", 78);
+
+                Queue<Employee> queue = new PriorityQueue<Employee>();
+                queue.add(janeJones);
+                queue.add(johnDoe);
+                queue.add(marySmith);
+                queue.add(mikeWilson);
+                queue.add(billEnd);
+                System.out.println("First init Queue");
+                for (Employee employee : queue) {
+                    System.out.println(employee.getId() + " " + employee.getFirstName() + " " + employee.getLastName());
+                }
+                System.out.println("\n");
+                System.out.println("Head Of Queue");
+                System.out.println(queue.element() + "--->> Using Element Method");
+                System.out.println(queue.peek() + "--->> Using Peek Method");
+
+                System.out.println("\n");
+                System.out.println("Remove Head Of Queue");
+                System.out.println(queue.remove(queue.element()));
+                for (Employee employee : queue) {
+                    System.out.println(employee.getId() + " " + employee.getFirstName() + " " + employee.getLastName());
+                }
+
+                System.out.println("\n");
+                System.out.println("Head Of Queue After Remove");
+                System.out.println(queue.element() + "--->> Using Element Method");
+                System.out.println(queue.peek() + "--->> Using Peek Method");
+
+                System.out.println("\n");
+                System.out.println("Add data to Queue via offer");
+                System.out.println(queue.offer(marySmith));
+                for (Employee employee : queue) {
+                    System.out.println(employee.getId() + " " + employee.getFirstName() + " " + employee.getLastName());
+                }
+
+                System.out.println("\n");
+                System.out.println("Get all data Queue");
+                Iterator<Employee> itr2 = queue.iterator();
+                while (itr2.hasNext()) {
+                    System.out.println(itr2.next());
+                }
+
+                System.out.println("\n\n");
+                next();
+                break;
+            default:
+                firstTask();
+        }
+    }
+
+    public static int linearSearch(String arr[], String x) {
+        int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            if (arr[i].equalsIgnoreCase(x)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(String arr[], String x) {
+
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m].equalsIgnoreCase(x)) {
+                return m;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void secondTask() {
+        int noSoal = 0;
+        String arr[] = {"ardi", "naufan", "hadian", "lidia", "gunawan"};
+        Scanner masukanSoal = new Scanner(System.in);
+        String soal = "1. Linear Search\n" + "2.Binary Search\n";
+        System.out.println(soal);
+        System.out.print("Masukan Pilihan : ");
+        noSoal = masukanSoal.nextInt();
+
+        switch (noSoal) {
+            case 1:
+                System.out.println(Arrays.toString(arr));
+                System.out.print("pilih nama : ");
+                Scanner s = new Scanner(System.in);
+                String x = s.nextLine();
+
+                // Function call
+                int result = linearSearch(arr, x);
+                if (result == -1) {
+                    System.out.print(
+                            "Nama Tidak Ditemukan!!");
+                } else {
+                    System.out.println("Nama Berada Pada Urutan array "
+                            + result + "(atau Pada Urutan ke " + (result + 1) + ")");
+                }
+                System.out.println("\n\n");
+                next();
+                break;
+            case 2:
+                System.out.println(Arrays.toString(arr));
+                System.out.print("pilih nama : ");
+                Scanner bs = new Scanner(System.in);
+                String bx = bs.nextLine();
+
+                // Function call
+                int bresult = binarySearch(arr, bx);
+                if (bresult == -1) {
+                    System.out.print(
+                            "Nama Tidak Ditemukan!!");
+                } else {
+                    System.out.println("Nama Berada Pada Urutan array "
+                            + bresult + "(atau Pada Urutan ke " + (bresult + 1) + ")");
+                }
+                System.out.println("\n\n");
+                next();
+                break;
+            default:
+                secondTask();
+        }
+    }
+
+    public static void thirdTask() {
+        Tree intTree = new Tree();
+
+        intTree.insert(30);
+        intTree.insert(23);
+        intTree.insert(19);
+        intTree.insert(24);
+        intTree.insert(44);
+        intTree.insert(87);
+        intTree.insert(54);
+        intTree.insert(41);
+        intTree.insert(52);
+        intTree.insert(7);
+        intTree.insert(3);
+        intTree.insert(8);
+
+        intTree.traverseInOrder();
+        System.out.println("\nSize of Binary Tree => "+ intTree.size());
+
+        System.out.println(intTree.get(24));
+        System.out.println(intTree.get(52));
+
+        System.out.println("Max Data Of Binary Tree => " + intTree.max());
+        System.out.println("Min Data Of Binary Tree => " + intTree.min());
+    }
+
+    public static void soal() {
+        int noSoal = 0;
+        Scanner masukanSoal = new Scanner(System.in);
+        String soal = "1.	Buat contoh implementasi stack dan queue pada coding program (lakukan show, insert, deletion pada stack dan queue) ?\n"
+                + "2.	Buat search program untuk array of string menggunakan (urutan ganjil iterative, urutan genap recursive function) untuk melakukan search pada array of string menggunakan linier search dan binary search\n"
+                + "3.	Buat contoh coding binary tree lakukan insert, traversalOrder, max, min dan menghitung element size dari binary tree?";
+        System.out.println(soal);
+        System.out.print("Masukan No Soal UAS : ");
+        noSoal = masukanSoal.nextInt();
+        switch (noSoal) {
+            case 1:
+                firstTask();
+                next();
+                break;
+            case 2:
+                secondTask();
+                next();
+                break;
+            case 3:
+                thirdTask();
+                next();
+                break;
+            case 4:
+                next();
+                break;
+            default:
+                soal();
+        }
+    }
+
+    static void next() {
+        Scanner lanjut = new Scanner(System.in);
+        System.out.println("Lanjut Atau Tidak?");
+        System.out.println("1. Yes\n" + "2. No");
+        System.out.print("Masukan Pilihan Anda : ");
+        int next = lanjut.nextInt();
+        if (next == 1) {
+            System.out.println("\n\n\n\n\n");
+            soal();
+        } else {
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        soal();
+    }
+
+}
